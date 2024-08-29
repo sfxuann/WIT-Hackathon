941d4e6f4776846986559907f05efcad71fa6468
tharsihav <tharsihav@gmail.com>
2024-08-29 12:43:14 +1000

    creating the vite and pages with wouter

--- hackathon/src/App.tsx
+++ hackathon/src/App.tsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Switch } from 'wouter';
+import ProfilePage from './pages/ProfilePage.tsx';
+import { Button, CssBaseline, CssVarsProvider } from '@mui/joy';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
+    <CssVarsProvider>
+      <CssBaseline />
+      <Switch>
+        <Route path="/" component={LandingPage} />
+        <Route path="/profile" component={ProfilePage} />
+      </Switch>
+    </CssVarsProvider>
+
+  )
+}
+
+const LandingPage = () => {
+  return (
     <>
+      <Button variant="solid">Hello world</Button>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>start page</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ hackathon/src/pages/ProfilePage.tsx
@@ -0,0 +1,11 @@
+import { Stack, Typography } from '@mui/joy';
+
+const ProfilePage = () => {
+  return (
+    <Stack height="100%" direction="row" justifyContent="space-evenly" alignItems="center">
+      <Typography level="body-lg" fontWeight="bold">Profile page :p </Typography>
+    </Stack>
+  );
+};
+
+export default ProfilePage;
